0a21615ab7737a228448f00566c0b884765b3ea3
RTPSticky <rtp.goodwork@gmail.com>
2026-01-21 09:45:53 +0700

    fix: แก้ไขชื่อการ์ดแดชบอร์ดจาก 'รายการอุบัติการณ์' เป็น 'รายงานอุบัติการณ์'

--- app/page.js
+++ app/page.js
@@ -137,7 +137,7 @@ export default async function Home() {
                 icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" /></svg>}
               />
               <DashboardCard
-                title="รายการอุบัติการณ์"
+                title="รายงานอุบัติการณ์"
                 description="รายงานการบาดเจ็บ/เสียชีวิตของเจ้าหน้าที่"
                 href="/incidents"
                 icon={<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" /></svg>}
